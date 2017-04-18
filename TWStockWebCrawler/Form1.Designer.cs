fd012274404494ff7d99eb2210a1158c720aa27b
JosephGang <lai225joseph@gmail.com>
2017-04-18 16:03:37 +0800

    essage

---
+++ TWStockWebCrawler/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TWStockWebCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboStock = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labStockPrice = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboStock
+            // 
+            this.tboStock.Location = new System.Drawing.Point(133, 25);
+            this.tboStock.Multiline = true;
+            this.tboStock.Name = "tboStock";
+            this.tboStock.Size = new System.Drawing.Size(174, 29);
+            this.tboStock.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "股票代碼 :";
+            // 
+            // labStockPrice
+            // 
+            this.labStockPrice.AutoSize = true;
+            this.labStockPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labStockPrice.Location = new System.Drawing.Point(18, 83);
+            this.labStockPrice.Name = "labStockPrice";
+            this.labStockPrice.Size = new System.Drawing.Size(0, 25);
+            this.labStockPrice.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "查詢股票";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(350, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labStockPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tboStock);
+            this.Name = "Form1";
+            this.Text = "股票查詢";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboStock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labStockPrice;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TWStockWebCrawler/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TWStockWebCrawler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetStockIndex()
+        {
+            return tboStock.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        async void DownloadPageAsync()
+        {
+            string page = "https://tw.stock.yahoo.com/q/q?s=" + GetStockIndex();
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(page))
+            using (HttpContent content = response.Content)
+            {
+                try
+                {
+                    string result = await content.ReadAsStringAsync();
+
+                    const string pattern = @"<td\b[^>]*?>(?<V>[\s\S]*?)</\s*td>";
+                    ArrayList arr = new ArrayList();
+                    foreach (Match match in Regex.Matches(result, pattern, RegexOptions.IgnoreCase))
+                    {
+                        if (match.Groups["V"].Value.Any(c => char.IsDigit(c)))
+                            arr.Add(match.Groups["V"].Value);
+                    }
+                    string stockPrize = arr[19].ToString();
+                    string stockBrand = arr[17].ToString();
+                    stockPrize = stockPrize.Replace("<b>", "").Replace("</b>", "");
+                    stockBrand = GetBrandName(stockBrand);
+                    Action<string> action = ReadStockPrice;
+                    Invoke(action, stockBrand + "股價=" + stockPrize);
+                }
+                catch
+                {
+                    MessageBox.Show("查詢錯誤，請確定輸入的股票代碼是否正確");
+                }
+            }
+        }
+        
+        private string GetBrandName(string stockBrand)
+        {
+            int index1 = stockBrand.IndexOf("</a>");
+            int index2 = stockBrand.IndexOf(GetStockIndex());
+            stockBrand = stockBrand.Substring(index2, index1 - index2);
+            int index3 = stockBrand.LastIndexOf(GetStockIndex());
+            stockBrand = stockBrand.Substring(index3, stockBrand.Length - index3);
+            for (int i = 0; i < stockBrand.Length; i++)
+            {
+                if(char.IsDigit(stockBrand[i]))
+            {
+                    stockBrand = stockBrand.Replace(stockBrand[i].ToString(), " ");
+            }
+            }
+            return stockBrand;
+        }
+
+        private void ReadStockPrice(string stockPrize)
+        {
+            labStockPrice.Text = stockPrize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(DownloadPageAsync);
+            t.Start();
+        }
+    }
+}
